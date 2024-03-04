d9d6212c819a7a70e752a54318bf71c681376a6c
xinyue.23@intl.zju.edu.cn <xinyue.23@intl.zju.edu.cn>
2024-03-04 22:43:51 +0800

    recursive.py_submission1

--- Practical4/recursive.py
+++ Practical4/recursive.py
@@ -1,9 +1,14 @@
 # generate a recursive sequence called a(n)
+# define a(n):
+# if n = 1, then return a(1)
+# if not, then return 2*a(n-1) + 3
 def a(n):
      if n == 1:
          return 4
      else:
          return 2*a(n-1) + 3
 # to have only the first five values
+# from n=1 to n=5 
+#      give me a(n) as I defined
 for n in range (1,6):
     print (a(n))
