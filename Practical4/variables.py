4bc4d20449c8651dce6d10bb99434e82f4dfa817
xinyue.23@intl.zju.edu.cn <xinyue.23@intl.zju.edu.cn>
2024-03-04 21:59:49 +0800

    variables.py_submmission

---
+++ Practical4/variables.py
@@ -0,0 +1,16 @@
+a = 40
+b = 36
+c = 30
+d = a-b
+e = b-c
+d <= e
+# Strength training had a greater improvement on running time.
+X = True
+Y = False
+W = X^Y
+print (W)
+# X     Y     either X or Y
+# True  True  False
+# True  False True
+# False True  True
+# False False False
