7801a1c531999676b509a1d0d8cf648ffb335344
xinyue.23@intl.zju.edu.cn <xinyue.23@intl.zju.edu.cn>
2024-04-15 18:03:24 +0800

    modification_based_on feedback2

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,7 +1,7 @@
 # The initial density is 5%
 density = 5
 # Begin with day 1
-day = 1
+day = 0
 #
 while density <= 90:
     # density double in a day
@@ -14,5 +14,5 @@ while density <= 90:
     else:
         # stop the cells from growing
         break
-print ("On day", day+1, "the density goes over 90%")
+print ("It takes", day+1, "days for the density to go over 90%")
 print ("The maximum number of days I can have a holiday from lab is", day)
