ed823937d23b5c1ae8fb74fd65d686ae74c95485
xinyue.23@intl.zju.edu.cn <xinyue.23@intl.zju.edu.cn>
2024-05-20 18:13:14 +0800

    duplicate_genes_submit

--- Practical8/count_repeat_duplicate_genes.py
+++ Practical8/count_repeat_duplicate_genes.py
@@ -3,26 +3,26 @@ fname = input_sequence + '_duplicate_genes.fa'
 xfile = open(fname, 'w')
 yfile = open('duplicate_genes.fa', 'r')
 import re
-gene_name = ""
-gene_sequence = ""
+name = ""
+sequence = ""
 for line in yfile:
     if line.startswith('>'):  
-        if gene_name and gene_sequence:  
-            count = len(re.findall(r'(?=(' + input_sequence + '))', gene_sequence))
+        if name and sequence:  
+            count = len(re.findall(r'(?=(' + input_sequence + '))', sequence)) #use '?=' to match overlapping occurrences of a repeated pattern.
             if count>0:
-                new_gene_name = f"{gene_name} {count}"
+                new_gene_name = f"{name} {count}"
                 xfile.write(f">{new_gene_name[1:]}\n")
-                xfile.write(f"{gene_sequence}\n")
-        gene_name = line.strip() 
-        gene_sequence = "" 
+                xfile.write(f"{sequence}\n")
+        name = line.strip() 
+        sequence = "" 
     else:
-        gene_sequence += line.strip()
-if gene_name:
-    count=len(re.findall(input_sequence, gene_sequence))
+        sequence += line.strip()
+if name:
+    count=len(re.findall(input_sequence, sequence))
     if count>0:
-        new_gene_name = f"{gene_name} {count}"
+        new_gene_name = f"{name} {count}"
         xfile.write(f">{new_gene_name[1:]}\n")
-        xfile.write(f"{gene_sequence}\n")
+        xfile.write(f"{sequence}\n")
 if xfile:
     xfile.close()
 if yfile:
