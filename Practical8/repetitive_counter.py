0e7be5495e2bae4b04dacf2aa54e5d80f7c438cc
xinyue.23@intl.zju.edu.cn <xinyue.23@intl.zju.edu.cn>
2024-05-20 18:04:13 +0800

    Practical8_final

--- Practical8/repetitive_counter.py
+++ Practical8/repetitive_counter.py
@@ -1,6 +1,6 @@
 import re
 seq = 'ATGCAATCGGTGTGTCTGTTCTGAGAGGGCCTAA'
-match_form1 = re.findall(r'GTGTGT', seq)
-match_form2 = re.findall(r'GTCTGT', seq)
+match_form1 = re.findall(r'(?=(GTGTGT))', seq) #use '?=' to match overlapping occurrences of a repeated pattern. For example, '…GTGTGTGT…’ has two instances of the repeat ‘GTGTGT’.
+match_form2 = re.findall(r'(?=(GTCTGT))', seq)
 total_number = len(match_form1) + len(match_form2)
 print("The total number of repeat elements:", total_number)
